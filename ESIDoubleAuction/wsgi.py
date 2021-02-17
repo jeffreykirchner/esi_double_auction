3ef1de5e86e31dd9ec707c91d736187416c793c5
Jeffrey Kirchner <jkirchner@gmail.com>
2021-02-17 14:29:27 -0800

    update name

--- ESIDoubleAuction/wsgi.py
+++ ESIDoubleAuction/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EIDoubleAuction project.
+WSGI config for ESIDoubleAuction project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EIDoubleAuction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESIDoubleAuction.settings')
 
 application = get_wsgi_application()
