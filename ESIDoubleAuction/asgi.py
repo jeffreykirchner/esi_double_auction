71c72652fb9bbd37c628e4f8d3babc17f9fa5d84
Jeffrey Kirchner <jkirchner@gmail.com>
2021-02-18 19:02:05 -0800

    formatting

--- ESIDoubleAuction/asgi.py
+++ ESIDoubleAuction/asgi.py
@@ -29,14 +29,32 @@
 #     ),
 # })
 
+# import os
+
+# from channels.routing import ProtocolTypeRouter
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESIDoubleAuction.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
+
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import main.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESIDoubleAuction.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ESIDoubleAuction.settings")
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
+    ),
 })
