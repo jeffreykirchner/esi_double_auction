03341edfbe03f5ffe9c97b680751e7de4c28fe4c
Jeffrey Kirchner <jkirchner@gmail.com>
2021-02-18 18:28:31 -0800

    formatting

--- ESIDoubleAuction/asgi.py
+++ ESIDoubleAuction/asgi.py
@@ -1,30 +1,42 @@
-"""
-ASGI config for ESIDoubleAuction project.
+# """
+# ASGI config for ESIDoubleAuction project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+# It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+# """
+# import os
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESIDoubleAuction.settings')
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+
+# import main.routing
+
+# #default implimentation
+# # application = get_asgi_application()
+
+# #channnels implimentation
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             main.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
 import os
+
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESIDoubleAuction.settings')
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-import main.routing
-
-#default implimentation
-# application = get_asgi_application()
-
-#channnels implimentation
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main.routing.websocket_urlpatterns
-        )
-    ),
+    # Just HTTP for now. (We can add other protocols later.)
 })
