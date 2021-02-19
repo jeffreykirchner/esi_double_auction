4bf4eca676abb5f805045205be305cdc9823dcd4
Jeffrey Kirchner <jkirchner@gmail.com>
2021-02-18 19:09:07 -0800

    formatting

--- ESIDoubleAuction/asgi.py
+++ ESIDoubleAuction/asgi.py
@@ -47,8 +47,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import main.routing
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ESIDoubleAuction.settings")
+
+django.setup()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
