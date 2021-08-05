34c6344ad59468b4a4d4618c7991c387c6fcf11d
Jeffrey Kirchner <jkirchner@gmail.com>
2021-08-05 15:08:40 -0700

    Buyer and seller paddles. fixes #26

--- main/views/staff_session_paddles.py
+++ main/views/staff_session_paddles.py
@@ -40,17 +40,32 @@ class StaffSessionPaddles(SingleObjectMixin, View):
         session = self.get_object()
 
         rows = []
+        col = []
 
         if kwargs['buyer_or_seller'] == "Buyer":
-            buyer_or_seller = "Buyer"
+            buyer_or_seller = "Buyer"          
+            buyer_or_seller_count = session.parameter_set.number_of_buyers
         else:
             buyer_or_seller = "Seller"
+            buyer_or_seller_count = session.parameter_set.number_of_sellers
+
+        for i in range(buyer_or_seller_count):
+
+            if buyer_or_seller == "Buyer":
+                col.append(f"B{i+1}")
+            else:
+                col.append(f"S{i+1}")
+
+            if i % 2 == 1 or i == buyer_or_seller_count-1:
+                rows.append(col)
+                col = []
 
         return render(request=request,
                       template_name=self.template_name,
                       context={"parameters" : parameters,
                                "id" : session.id,
                                "buyer_or_seller" : buyer_or_seller,
+                               "buyer_or_seller_count" : buyer_or_seller_count,
                                "rows" : rows,
                                "session" : session})
     
