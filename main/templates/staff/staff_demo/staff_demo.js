3d00befc3b4d895d14970ac3cfcb93ed23cc858d
Jeffrey Kirchner <kirchner@chapman.edu>
2022-08-15 16:28:44 -0700

    update demo sheet. fixes #95.

--- main/templates/staff/staff_demo/staff_demo.js
+++ main/templates/staff/staff_demo/staff_demo.js
@@ -51,7 +51,7 @@ var app = Vue.createApp({
             }
             else
             {
-                app.$data.costs_profits[index] = parseFloat(app.$data.seller.periods[0].cost_list[index-1].value_cost) - app.$data.costs_prices[index];
+                app.$data.costs_profits[index] =  app.$data.costs_prices[index] - parseFloat(app.$data.seller.periods[0].cost_list[index-1].value_cost);
                 app.$data.costs_profits[index] = app.$data.costs_profits[index].toFixed(2);
                 app.$data.costs_prices[index] = parseFloat(app.$data.costs_prices[index]).toFixed(2);
             }
